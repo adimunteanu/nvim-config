08193239c5ce1a4184a4f5f3ad268f3fa5f15518
adimunteanu <adimunteanu13@gmail.com>
2022-08-07 21:58:14 +0200

    Add working base for nvim config with CoC

---
+++ lua/user/coc.lua
@@ -0,0 +1,26 @@
+vim.cmd [[
+" navigate between options with tab and shift tab
+inoremap <expr> <Tab> coc#pum#visible() ? coc#pum#next(1) : "\<Tab>"
+inoremap <expr> <S-Tab> coc#pum#visible() ? coc#pum#prev(1) : "\<S-Tab>"
+
+" remap enter to confirm entry
+inoremap <expr> <cr> coc#pum#visible() ? coc#pum#confirm() : "\<CR>"
+
+" use <tab> for trigger completion and navigate to the next complete item
+function! CheckBackspace() abort
+  let col = col('.') - 1
+  return !col || getline('.')[col - 1]  =~# '\s'
+endfunction
+
+inoremap <silent><expr> <Tab>
+      \ coc#pum#visible() ? coc#pum#next(1) :
+      \ CheckBackspace() ? "\<Tab>" :
+      \ coc#refresh()
+" Remap keys for gotos
+nmap <silent> gd <Plug>(coc-definition)
+nmap <silent> gy <Plug>(coc-type-definition)
+nmap <silent> gi <Plug>(coc-implementation)
+nmap <silent> gr <Plug>(coc-references)
+" Auto organize imports on saving buffer
+autocmd BufWritePre *.ts,*.js,*.go,*.tsx :call CocAction('runCommand', 'editor.action.organizeImport') | sleep 100m
+]]
