b66ca98db52b7f4213523796f90c562dc269f580
adimunteanu <adimunteanu13@gmail.com>
2022-08-08 13:42:17 +0200

    Add nvim-tree plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -70,6 +70,10 @@ return packer.startup(function(use)
   use "p00f/nvim-ts-rainbow"
   use 'JoosepAlviste/nvim-ts-context-commentstring'
 
+  -- Nvim-tree
+  use 'kyazdani42/nvim-web-devicons'
+  use 'kyazdani42/nvim-tree.lua'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
