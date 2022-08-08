fe5e9e8c4a4f6d699a206309e2ccb53d57cad861
adimunteanu <adimunteanu13@gmail.com>
2022-08-08 13:25:44 +0200

    Add comment plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,6 +45,7 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- Autopairs
+  use "numToStr/Comment.nvim" -- Easily comment stuff
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
@@ -64,6 +65,7 @@ return packer.startup(function(use)
 		commit = "518e27589c0463af15463c9d675c65e464efc2fe",
 	})
   use "p00f/nvim-ts-rainbow"
+  use 'JoosepAlviste/nvim-ts-context-commentstring'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
